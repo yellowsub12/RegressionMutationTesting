02a8381acc6bee9105edb939523e081d6b815498
yellowsub12 <fantasm1337@gmail.com>
2023-04-05 13:08:56 -0400

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            System.out.println("Hello world!");
+
+    }
+
+    public static String hello(int x){
+        if (x==0) {
+           return "Hello world!";
+        };
+        return null;
     }
 }
---
+++ src/test/java/QuickSortTest.java
@@ -0,0 +1,30 @@
+import org.example.Main;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class QuickSortTest {
+
+    @Test
+    @DisplayName("MainTest")
+    void mainTest() throws Exception {
+        String hello = "Hello world!";
+        assertEquals(Main.hello(0), hello);
+    }
+
+
+}
